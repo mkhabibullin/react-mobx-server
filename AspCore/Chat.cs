6423c23051bee797084191cddcb453bebc427aad
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-02-07 17:41:25 +0300

    Init

---
+++ AspCore/Chat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCore
+{
+    public class Chat : Hub
+    {
+        public async Task send(string nick, string message)
+        {
+            await Clients.All.SendAsync("Send", message);
+        }
+    }
+}
