fee63f075b995a81de43d151647aa168c084a064
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-03 18:53:25 +0300

    NLog

--- TimeReport/Program.cs
+++ TimeReport/Program.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace TimeReport
 {
@@ -14,39 +16,60 @@ namespace TimeReport
 
         public static void Main(string[] args)
         {
-            var isService = !(Debugger.IsAttached || args.Contains("--console"));
-
-            if (isService)
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
             {
-                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
-                Directory.SetCurrentDirectory(pathToContentRoot);
+                var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+                if (isService)
+                {
+                    var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                    var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                    Directory.SetCurrentDirectory(pathToContentRoot);
+                }
+
+                var configBuilder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json");
+                Configuration = configBuilder.Build();
+
+                var builder = CreateWebHostBuilder(
+                    args.Where(arg => arg != "--console").ToArray());
+
+                var host = builder.Build();
+
+                if (isService)
+                {
+                    // To run the app without the CustomWebHostService change the
+                    // next line to host.RunAsService();
+                    host.RunAsCustomService();
+                }
+                else
+                {
+                    host.Run();
+                }
             }
-
-            var configBuilder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json");
-            Configuration = configBuilder.Build();
-
-            var builder = CreateWebHostBuilder(
-                args.Where(arg => arg != "--console").ToArray());
-
-            var host = builder.Build();
-
-            if (isService)
+            catch (Exception ex)
             {
-                // To run the app without the CustomWebHostService change the
-                // next line to host.RunAsService();
-                host.RunAsCustomService();
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
             }
-            else
+            finally
             {
-                host.Run();
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
             }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseUrls(Configuration.GetValue<string>("Urls"))
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
