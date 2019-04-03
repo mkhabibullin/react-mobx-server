3c67da4023c07813f7611f85a9301012f93cac96
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-03 16:25:34 +0300

    test

--- TimeReport/Domain/User.cs
+++ TimeReport/Domain/User.cs
@@ -2,21 +2,26 @@
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDbRepository;
 using MongoDbRepository.Helper;
+using System.Runtime.Serialization;
 
 namespace TimeReport.Domain
 {
     [CollectionName("Users")]
     public class User : Entity
     {
+        [DataMember]
         [BsonElement("name")]
         public string Name { get; set; }
 
+        [DataMember]
         [BsonElement("blog")]
         public string Blog { get; set; }
 
+        [DataMember]
         [BsonElement("age")]
         public int Age { get; set; }
 
+        [DataMember]
         [BsonElement("location")]
         public string Location { get; set; }
     }
