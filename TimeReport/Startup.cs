8959f9c21ff5ab5a28805af7bf643066e1ec7066
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-03 15:26:31 +0300

    Disabled https

--- TimeReport/Startup.cs
+++ TimeReport/Startup.cs
@@ -50,7 +50,7 @@ namespace TimeReport
             }
             else
             {
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             //app.UseHttpsRedirection();
