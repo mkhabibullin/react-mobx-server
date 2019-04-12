511429a1841ba30c966aeeea5c193dde5d0d908f
Хабибуллин Марат Мансурович <marat-011@yandex.ru>
2019-04-12 23:35:17 +0300

    Ocelot as a service

--- ApiGetway/Program.cs
+++ ApiGetway/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Ocelot.Middleware;
-using Ocelot.DependencyInjection;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using TimeReport;
 
 namespace ApiGetway
 {
@@ -16,9 +13,30 @@ namespace ApiGetway
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args)
-                .Build()
-                .Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+            }
+
+            var builder = CreateWebHostBuilder(
+            args.Where(arg => arg != "--console").ToArray());
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                // To run the app without the CustomWebHostService change the
+                // next line to host.RunAsService();
+                host.RunAsCustomService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
---
+++ ApiGetway/WebHost/WebHostServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ApiGetway;
+using Microsoft.AspNetCore.Hosting;
+using System.ServiceProcess;
+
+namespace TimeReport
+{
+    public static class WebHostServiceExtensions
+    {
+        public static void RunAsCustomService(this IWebHost host)
+        {
+            var webHostService = new CustomWebHostService(host);
+            ServiceBase.Run(webHostService);
+        }
+    }
+}
