d6a12c3da52c583392410eb2193e426c35bd6af8
mkhabibullin <safe.kzn@gmail.com>
2019-07-12 12:59:51 +0300

    +

--- TimeReport/Extensions/AutoMapperExtensions.cs
+++ TimeReport/Extensions/AutoMapperExtensions.cs
@@ -1,14 +1,18 @@
 ﻿using AutoMapper;
 using NSpecifications;
-using static TimeReport.Profiles.TimeTrackingProfile;
 
 namespace TimeReport.Extensions
 {
     public static class AutoMapperExtensions
     {
+        public static string SpecOption => "Spec";
+
         public static T MapWithSpec<T, S>(this IMapper mapper, object source, Spec<S> spec)
         {
             return mapper.Map<T>(source, opt => opt.Items[SpecOption] = spec);
         }
+
+        public static Spec<T> BySpecOf<T>(this ResolutionContext context)
+            => context.Items[SpecOption] as Spec<T>;
     }
 }
--- TimeReport/Profiles/TimeTrackingProfile.cs
+++ TimeReport/Profiles/TimeTrackingProfile.cs
@@ -1,24 +1,22 @@
 ﻿using AutoMapper;
-using NSpecifications;
 using System;
 using System.Linq;
 using TimeReport.Dto;
 using TimeReport.Dto.Jira;
+using TimeReport.Extensions;
 
 namespace TimeReport.Profiles
 {
     public class TimeTrackingProfile : Profile
     {
-        public static string SpecOption => "Spec";
-
         public TimeTrackingProfile()
         {
             CreateMap<JiraWorkLogItemDto, TimeTrackingTaskItemDto>()
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => DateTime.Parse(src.Created)));
 
             CreateMap<TimeTrackingTaskDto, TimeReportItemDto>()
-                .ForMember(dest => dest.Hours, opt => opt.MapFrom((src, d, s, context) => 
-                    src.Itmes.Where(context.Items[SpecOption] as Spec<TimeTrackingTaskItemDto>)
+                .ForMember(dest => dest.Hours, opt => opt.MapFrom((src, d, s, context) => src.Itmes
+                    .Where(context.BySpecOf<TimeTrackingTaskItemDto>())
                     .Aggregate(0f, (sum, i) => sum + (i.TimeSpentSeconds / 60f / 60f))));
         }
     }
